3902ff434a486b574ab26df9856f34be6344cadc
foglii <100421246+foglii@users.noreply.github.com>
2025-04-14 14:08:54 +0200

    Update first_try.py

--- first_try.py
+++ first_try.py
@@ -8,7 +8,7 @@ import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
-from cflib.positioning.motion_commander import PositionHlCommander
+from cflib.positioning.position_hl_commander import PositionHlCommander
 from cflib.utils import uri_helper
 
 URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
