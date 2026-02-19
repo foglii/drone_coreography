baedcd2d00fcb3282d23dee46e18e35b3039f9e0
foglii <chiarabarone987@gmail.com>
2026-02-19 23:43:55 +0100

    multiple trajectories per drone, load half of the trajectory for each drone

--- coreo.py
+++ coreo.py
@@ -23,43 +23,45 @@ URI4 = 'radio://0/80/2M/E7E7E7E7E9'
 URI5 = 'radio://0/80/2M/E7E7E7E7E5'
 URI6 = 'radio://0/80/2M/E7E7E7E7E4'
 
-
-# drone1 = [list(map(float, row)) for row in __import__("csv").reader(open("drone1.csv", encoding="utf-8-sig"))]
-# drone2 = [list(map(float, row)) for row in __import__("csv").reader(open("drone2.csv", encoding="utf-8-sig"))]
-# drone3 = [list(map(float, row)) for row in __import__("csv").reader(open("drone3.csv", encoding="utf-8-sig"))]
-# drone4 = [list(map(float, row)) for row in __import__("csv").reader(open("drone4.csv", encoding="utf-8-sig"))]
-# drone5 = [list(map(float, row)) for row in __import__("csv").reader(open("drone5.csv", encoding="utf-8-sig"))]
-# drone6 = [list(map(float, row)) for row in __import__("csv").reader(open("drone6.csv", encoding="utf-8-sig"))]
 global iterr
 iterr= 1
-dronee1= uav_trajectory.Trajectory()
-dronee1.loadcsv('drone1.csv')
-dronee2= uav_trajectory.Trajectory()
-dronee2.loadcsv('drone2.csv')
-dronee3= uav_trajectory.Trajectory()
-dronee3.loadcsv('drone3.csv')
-dronee4= uav_trajectory.Trajectory()
-dronee4.loadcsv('drone4.csv')
-dronee5= uav_trajectory.Trajectory()
-dronee5.loadcsv('drone5.csv')
-dronee6= uav_trajectory.Trajectory()
-dronee6.loadcsv('drone6.csv')
-droneee1 = np.loadtxt('drone1.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
-drone11=droneee1.tolist()
-drone2 = np.loadtxt('drone2.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
-drone2=np.asarray(drone2,dtype=float)
-droneee3 = np.loadtxt('drone3.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
-drone33=droneee3.tolist()
-drone3=drone11[:len(drone33)//2]
-drone3_2=drone11[len(drone33)//2+1:]
-droneee4 = np.loadtxt('drone4.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
-drone44=droneee4.tolist()
-drone4=drone44[:len(drone44)//2]
-drone4_2=drone44[len(drone44)//2+1:]
-drone5 = np.loadtxt('drone5.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
-drone6 = np.loadtxt('drone6.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
-drone1=drone11[:len(drone11)//2]
-drone1_2=drone11[len(drone11)//2:]
+# dronee1= uav_trajectory.Trajectory()
+# dronee1.loadcsv('drone1.csv')
+# dronee2= uav_trajectory.Trajectory()
+# dronee2.loadcsv('drone2.csv')
+# dronee3= uav_trajectory.Trajectory()
+# dronee3.loadcsv('drone3.csv')
+# dronee4= uav_trajectory.Trajectory()
+# dronee4.loadcsv('drone4.csv')
+# dronee5= uav_trajectory.Trajectory()
+# dronee5.loadcsv('drone5.csv')
+# dronee6= uav_trajectory.Trajectory()
+# dronee6.loadcsv('drone6.csv')
+drone1_txt = np.loadtxt('drone1.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
+drone1=drone1_txt[:len(drone1_txt)//2].tolist()
+drone1_2=drone1_txt[len(drone1_txt)//2:].tolist()
+drone1_duration = [np.sum(np.array(drone1)[:,0]), np.sum(np.array(drone1_2)[:,0])]
+drone2_txt = np.loadtxt('drone2.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
+drone2=drone2_txt[:len(drone2_txt)//2].tolist()
+drone2_2=drone2_txt[len(drone2_txt)//2:].tolist()
+drone2_duration = [np.sum(np.array(drone2)[:,0]), np.sum(np.array(drone2_2)[:,0])]
+drone3_txt = np.loadtxt('drone3.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
+drone3=drone3_txt[:len(drone3_txt)//2].tolist()
+drone3_2=drone3_txt[len(drone3_txt)//2:].tolist()
+drone3_duration = [np.sum(np.array(drone3)[:,0]), np.sum(np.array(drone3_2)[:,0])]
+drone4_txt = np.loadtxt('drone4.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
+drone4=drone4_txt[:len(drone4_txt)//2].tolist()
+drone4_2=drone4_txt[len(drone4_txt)//2:].tolist()
+drone4_duration = [np.sum(np.array(drone4)[:,0]), np.sum(np.array(drone4_2)[:,0])]
+drone5_txt = np.loadtxt('drone5.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
+drone5=drone5_txt[:len(drone5_txt)//2].tolist()
+drone5_2=drone5_txt[len(drone5_txt)//2:].tolist()
+drone5_duration = [np.sum(np.array(drone5)[:,0]), np.sum(np.array(drone5_2)[:,0])]
+drone6_txt = np.loadtxt('drone6.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
+drone6=drone6_txt[:len(drone6_txt)//2].tolist()
+drone6_2=drone6_txt[len(drone6_txt)//2:].tolist()
+drone6_duration = [np.sum(np.array(drone6)[:,0]), np.sum(np.array(drone6_2)[:,0])]
+
 seq_args_ = {
     #URI1: [1,drone1],
    # URI2: [2,drone2],
@@ -74,21 +76,27 @@ seq_args_2 = {
      #URI2: [2,drone2_2],
      URI3: [3,drone3_2],
      URI4: [4,drone4_2],
-    #URI5: [5,drone5],
-    #URI6: [6,drone6],
+    #URI5: [5,drone5_2],
+    #URI6: [6,drone6_2],
     
 }
 seq_argss_ = {
-    #URI1: [1,dronee1.duration/2],
-     #URI2: [2,60],
-    URI3: [3,dronee3.duration/2],
-    URI4: [4,dronee4.duration/2],
-    #URI5: [5,dronee5.duration],
-    #URI6: [6,11.1],
-    #URI7: [sequence7],
-    #URI8: [sequence8],
-    #URI9: [sequence9],
-    #URI10: [sequence10],
+    #URI1: [1,drone1_duration[0]],
+    #URI2: [2,drone2_duration[0]],
+    URI3: [3,drone3_duration[0]],
+    URI4: [4,drone4_duration[0]],
+    #URI5: [5,drone5_duration[0]],
+    #URI6: [6,drone6_duration[0]],
+
+}
+seq_argss_2 = {
+    #URI1: [1,drone1_duration[0]],
+    #URI2: [2,drone2_duration[0]],
+    URI3: [3,drone3_duration[1]],
+    URI4: [4,drone4_duration[1]],
+    #URI5: [5,drone5_duration[0]],
+    #URI6: [6,drone6_duration[0]],
+
 }
 
 # List of URIs, comment the one you do not want to fly
@@ -99,10 +107,7 @@ uris = {
      URI4,
    # URI5,
    # URI6,
-   # URI7,
-   # URI8,
-   # URI9,
-   # URI10
+
 }
 
 #######
@@ -160,13 +165,14 @@ def run_trajectory(scf, trajectory_id, duration):
             time.sleep(3.0)
         commander.start_trajectory(trajectory_id, 1.5, False)
         print(f"[{scf.cf.link_uri}] Running trajectory {trajectory_id} for {duration:.1f}s")
-        time.sleep(duration+1)
+        time.sleep(duration)
        
         if iterr==2:
                 commander.land(0.0, 2.0)
                 time.sleep(3.0)
                 commander.stop()
-        else: commander.up(0.2,0.1)
+        else:  commander.go_to(0, 0, 0.1, 0, 1, relative=True, linear=True)
+             
     except Exception as e:
         print(f"[{scf.cf.link_uri}] ERROR uploading trajectory: {e}")
         commander.land(0.0, 2.0)
@@ -211,10 +217,8 @@ if __name__ == '__main__':
             swarm.parallel_safe(run_trajectory, args_dict=seq_argss_)
             print("ciao")
             iterr= iterr+1
-            swarm.parallel_safe(wait_for_param_download)
-            
             durations = swarm.parallel_safe(upload_trajectory, args_dict=seq_args_2)
-            swarm.parallel_safe(run_trajectory, args_dict=seq_argss_)
+            swarm.parallel_safe(run_trajectory, args_dict=seq_argss_2)
         except KeyboardInterrupt:
             print("Emergency stop triggered by user.")
             swarm.parallel_safe(landd)
@@ -222,182 +226,3 @@ if __name__ == '__main__':
             print(f"Errorr: {e}")
             swarm.parallel_safe(landd)
 
-""" def activate_mellinger_controller(cf):
-    cf.param.set_value('stabilizer.controller', '2')
-
-
-def upload_trajectory(cf, trajectory_id, trajectory):
-    try:
-        print('hello')
-        trajectory_mem = cf.cf.mem.get_mems(MemoryElement.TYPE_TRAJ)[0]
-        trajectory_mem.trajectory = []
-
-        total_duration = 0
-        for row in trajectory:
-            duration = row[0]
-            x = Poly4D.Poly(row[1:9])
-            y = Poly4D.Poly(row[9:17])
-            z = Poly4D.Poly(row[17:25])
-            yaw = Poly4D.Poly(row[25:33])
-            trajectory_mem.trajectory.append(Poly4D(duration, x, y, z, yaw))
-            total_duration += duration
-
-        upload_result = trajectory_mem.write_data_sync()
-        print('Uploaded')
-        if not upload_result:
-            print('Upload failed, aborting!')
-            sys.exit(1)
-        cf.cf.high_level_commander.define_trajectory(trajectory_id, 0, len(trajectory_mem.trajectory))
-        print(total_duration)
-        return total_duration
-    except Exception as e:
-        print(e)
-
-#####
-def activate_led_bit_mask(scf):
-    scf.cf.param.set_value('led.bitmask', 255)
-
-def deactivate_led_bit_mask(scf):
-    scf.cf.param.set_value('led.bitmask', 0)
-
-def light_check(scf):
-    activate_led_bit_mask(scf)
-    time.sleep(2)
-    deactivate_led_bit_mask(scf)
-
-def wait_for_param_download(scf):
-    while not scf.cf.param.is_updated:
-        time.sleep(1.0)
-    print('Parameters downloaded for', scf.cf.link_uri)
-
-
-def arm(scf):
-    scf.cf.platform.send_arming_request(True)
-    time.sleep(1.0)
-
-
-def take_off(cf, position):
-    take_off_time = 1.0
-    sleep_time = 0.1
-    steps = int(take_off_time / sleep_time)
-    vz = position[2] / take_off_time
-
-    print(vz)
-
-    for i in range(steps):
-        cf.commander.send_velocity_world_setpoint(0, 0, vz, 0)
-        time.sleep(sleep_time)
-
-
-def land(cf, position):
-    landing_time = 1.0
-    sleep_time = 0.1
-    steps = int(landing_time / sleep_time)
-    vz = -position[2] / landing_time
-
-    print(vz)
-
-    for _ in range(steps):
-        cf.commander.send_velocity_world_setpoint(0, 0, vz, 0)
-        time.sleep(sleep_time)
-
-    cf.commander.send_stop_setpoint()
-    # Hand control over to the high level commander to avoid timeout and locking of the Crazyflie
-    cf.commander.send_notify_setpoint_stop()
-
-    # Make sure that the last packet leaves before the link is closed
-    # since the message queue is not flushed before closing
-    time.sleep(0.1)
-
-
-def run_sequence(scf, sequence):
-    try:
-        cf=scf.cf
-
-        take_off(scf, sequence[0])
-        for position in sequence:
-            print('Setting position {}'.format(position))
-            end_time = time.time() + position[3]
-            while time.time() < end_time:
-                cf.commander.send_position_setpoint(position[0],
-                                                    position[1],
-                                                    position[2], 0)
-                time.sleep(0.1)
-        land(scf, sequence[-1])
-    except Exception as e:
-        print(e)
-
-def run_sequencee(scf, trajectory_id,figure8):
-    try:
-        print('run function')
-        commander = scf.cf.high_level_commander
-        relative_yaw=False
-        print('second print')
-        duration=21
-        #duration = upload_trajectory(cf, trajectory_id, figure8)
-        print(duration)
-        print('The sequence is {:.1f} seconds long'.format(duration))
-        # Arm the Crazyflie
-        #cf.platform.send_arming_request(True)
-        print('third print')
-        time.sleep(1.0)
-
-        takeoff_yaw = 3.14 / 2 if relative_yaw else 0.0
-        commander.takeoff(0.5, 2.0, yaw=takeoff_yaw)
-        time.sleep(3.0)
-        commander.start_trajectory(trajectory_id, 1.0, True)
-        time.sleep(duration)
-        commander.land(0.0, 2.0)
-        time.sleep(2)
-        commander.stop()
-    except Exception as e:
-        print(e)
-
-
-if __name__ == '__main__':
-    # logging.basicConfig(level=logging.DEBUG)
-    cflib.crtp.init_drivers()
-
-    factory = CachedCfFactory(rw_cache='./cache')
-    with Swarm(uris, factory=factory) as swarm:
-        try:
-            # If the copters are started in their correct positions this is
-            # probably not needed. The Kalman filter will have time to converge
-            # any way since it takes a while to start them all up and connect. We
-            # keep the code here to illustrate how to do it.
-            
-
-            # The current values of all parameters are downloaded as a part of the
-            # connections sequence. Since we have 10 copters this is clogging up
-            # communication and we have to wait for it to finish before we start
-            # flying.
-            swarm.parallel_safe(light_check)
-
-            durat=swarm.parallel_safe(upload_trajectory,args_dict=seq_args_)
-            
-            print(durat)
-            #swarm.parallel(wait_for_param_download)
-            time.sleep(5)
-
-            swarm.reset_estimators()
-        
-        
-
-            swarm.parallel_safe(activate_mellinger_controller)
-            
-            
-        
-            
-            
-            swarm.parallel_safe(arm)
-            #swarm.parallel(run_sequence,args_dict=seq_args_)
-            swarm.parallel_safe(run_sequencee,args_dict=seq_args_)
-
-        # swarm.parallel(run_sequence, args_dict=seq_args)
-        except Exception as e:
-                print(f"Error occurred: {e}")
-                swarm.parallel_safe(emergency_stop())
-
-
-# --- Helper functions ---
- """
