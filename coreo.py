ecf5ecf403b914e65cf11e3b61afd11b6bfa69d0
foglii <100421246+foglii@users.noreply.github.com>
2026-02-20 12:09:10 +0100

    Update URIs and adjust drone duration calculations

--- coreo.py
+++ coreo.py
@@ -16,7 +16,7 @@ from cflib.utils import uri_helper
 
 # Change uris and sequences according to your setup
 # URIs in a swarm using the same radio must also be on the same channel
-URI1 = 'radio://0/80/2M/E7E7E7E7E5'
+URI1 = 'radio://0/80/2M/E7E7E7E7E6'
 URI2 = 'radio://0/80/2M/E7E7E7E7E7'
 URI3 = 'radio://0/80/2M/E7E7E7E7E8'
 URI4 = 'radio://0/80/2M/E7E7E7E7E9'
@@ -39,11 +39,11 @@ iterr= 1
 # dronee6.loadcsv('drone6.csv')
 drone1_txt = np.loadtxt('drone1.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
 drone1=drone1_txt[:len(drone1_txt)//2].tolist()
-drone1_2=drone1_txt[len(drone1_txt)//2:].tolist()
+drone1_2=drone1_txt[len(drone1_txt)//2+1:].tolist()
 drone1_duration = [np.sum(np.array(drone1)[:,0]), np.sum(np.array(drone1_2)[:,0])]
 drone2_txt = np.loadtxt('drone2.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
 drone2=drone2_txt[:len(drone2_txt)//2].tolist()
-drone2_2=drone2_txt[len(drone2_txt)//2:].tolist()
+drone2_2=drone2_txt[len(drone2_txt)//2+1:].tolist()
 drone2_duration = [np.sum(np.array(drone2)[:,0]), np.sum(np.array(drone2_2)[:,0])]
 drone3_txt = np.loadtxt('drone3.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
 drone3=drone3_txt[:len(drone3_txt)//2].tolist()
@@ -51,29 +51,29 @@ drone3_2=drone3_txt[len(drone3_txt)//2:].tolist()
 drone3_duration = [np.sum(np.array(drone3)[:,0]), np.sum(np.array(drone3_2)[:,0])]
 drone4_txt = np.loadtxt('drone4.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
 drone4=drone4_txt[:len(drone4_txt)//2].tolist()
-drone4_2=drone4_txt[len(drone4_txt)//2:].tolist()
+drone4_2=drone4_txt[len(drone4_txt)//2+1:].tolist()
 drone4_duration = [np.sum(np.array(drone4)[:,0]), np.sum(np.array(drone4_2)[:,0])]
 drone5_txt = np.loadtxt('drone5.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
 drone5=drone5_txt[:len(drone5_txt)//2].tolist()
-drone5_2=drone5_txt[len(drone5_txt)//2:].tolist()
+drone5_2=drone5_txt[len(drone5_txt)//2+1:].tolist()
 drone5_duration = [np.sum(np.array(drone5)[:,0]), np.sum(np.array(drone5_2)[:,0])]
 drone6_txt = np.loadtxt('drone6.csv', delimiter=",", skiprows=1, usecols=range(33), ndmin=2)
 drone6=drone6_txt[:len(drone6_txt)//2].tolist()
-drone6_2=drone6_txt[len(drone6_txt)//2:].tolist()
+drone6_2=drone6_txt[len(drone6_txt)//2+1:].tolist()
 drone6_duration = [np.sum(np.array(drone6)[:,0]), np.sum(np.array(drone6_2)[:,0])]
 
 seq_args_ = {
-    #URI1: [1,drone1],
-   # URI2: [2,drone2],
+    URI1: [1,drone1],
+    URI2: [2,drone2],
     URI3: [3,drone3],
     URI4: [4,drone4],
-    #URI5: [5,drone5],
+   # URI5: [5,drone5],
     #URI6: [6,drone6],
     
 }
 seq_args_2 = {
-     #URI1: [1,drone1_2],
-     #URI2: [2,drone2_2],
+     URI1: [1,drone1_2],
+     URI2: [2,drone2_2],
      URI3: [3,drone3_2],
      URI4: [4,drone4_2],
     #URI5: [5,drone5_2],
@@ -81,8 +81,8 @@ seq_args_2 = {
     
 }
 seq_argss_ = {
-    #URI1: [1,drone1_duration[0]],
-    #URI2: [2,drone2_duration[0]],
+    URI1: [1,drone1_duration[0]],
+    URI2: [2,drone2_duration[0]],
     URI3: [3,drone3_duration[0]],
     URI4: [4,drone4_duration[0]],
     #URI5: [5,drone5_duration[0]],
@@ -90,8 +90,8 @@ seq_argss_ = {
 
 }
 seq_argss_2 = {
-    #URI1: [1,drone1_duration[0]],
-    #URI2: [2,drone2_duration[0]],
+    URI1: [1,drone1_duration[0]],
+    URI2: [2,drone2_duration[0]],
     URI3: [3,drone3_duration[1]],
     URI4: [4,drone4_duration[1]],
     #URI5: [5,drone5_duration[0]],
@@ -101,11 +101,11 @@ seq_argss_2 = {
 
 # List of URIs, comment the one you do not want to fly
 uris = {
-   #URI1,
-     #URI2,
+   URI1,
+     URI2,
     URI3,
      URI4,
-   # URI5,
+    #URI5,
    # URI6,
 
 }
@@ -163,15 +163,15 @@ def run_trajectory(scf, trajectory_id, duration):
         if iterr==1:
             commander.takeoff(0.5, 2.0)
             time.sleep(3.0)
-        commander.start_trajectory(trajectory_id, 1.5, False)
+        commander.start_trajectory(trajectory_id, 2, False)
         print(f"[{scf.cf.link_uri}] Running trajectory {trajectory_id} for {duration:.1f}s")
-        time.sleep(duration)
+        time.sleep(duration*2)
        
         if iterr==2:
                 commander.land(0.0, 2.0)
                 time.sleep(3.0)
                 commander.stop()
-        else:  commander.go_to(0, 0, 0.1, 0, 1, relative=True, linear=True)
+        else:  commander.go_to(0, 0, 0.01, 0, 1, relative=True, linear=True)
              
     except Exception as e:
         print(f"[{scf.cf.link_uri}] ERROR uploading trajectory: {e}")
@@ -201,7 +201,7 @@ if __name__ == '__main__':
             swarm.parallel_safe(wait_for_param_download)
             swarm.parallel_safe(light_check)
             swarm.reset_estimators()
-            swarm.parallel_safe(activate_mellinger_controller)
+            #swarm.parallel_safe(activate_mellinger_controller)
 
             # Upload trajectories to each Crazyflie
             durations = swarm.parallel_safe(upload_trajectory, args_dict=seq_args_)
@@ -225,4 +225,3 @@ if __name__ == '__main__':
         except Exception as e:
             print(f"Errorr: {e}")
             swarm.parallel_safe(landd)
-
